835a1901b0436779e4794e02a1c1375cfccdca7e
divyanshus020 <sharmadivyanshu281@gmail.com>
2023-12-12 23:55:47 +0530

    upload it

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,6 +21,8 @@ window.addEventListener("load", function () {
  * add event listener on multiple elements
  */
 
+
+
 const addEventOnElements = function (elements, eventType, callback) {
   for (let i = 0, len = elements.length; i < len; i++) {
     elements[i].addEventListener(eventType, callback);
